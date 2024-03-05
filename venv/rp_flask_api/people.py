e656edc764545ced279f5e2a79f1009a05783903
Bill <willjsaunders@icloud.com>
2024-03-05 20:21:14 +0000

    add update function

--- venv/rp_flask_api/people.py
+++ venv/rp_flask_api/people.py
@@ -1,6 +1,7 @@
 # people.py
 
 from datetime import datetime
+from flask import abort
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -25,3 +26,50 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname], 201
+    else:
+        abort(
+            406, "Person with last name {lname} already exists",
+        )
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
+
+# create a new person by passing a dictionary to the create() function. returns a tuple where the first element is the created person's dictionary and the second element is the HTTP status code.
+new_person = {
+    "lname": "Doe",
+    "fname": "John"
+}
+result, status_code = create(new_person)
+
+# Call the update function
+updated_person = {
+    "fname": "Jane"
+}
+result = update("Ruprecht", updated_person)
